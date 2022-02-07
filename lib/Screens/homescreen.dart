b79046034edf1e48bc5613c6558066c18aba8be5
unknown <shebinpr126@gmail.com>
2022-02-07 12:31:09 +0530

    registration_page

--- lib/Screens/homescreen.dart
+++ lib/Screens/homescreen.dart
@@ -15,21 +15,144 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        appBar: AppBar(),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: const Text(
+            "Register your shop",
+            style: TextStyle(color: Colors.black),
+          ),
+          centerTitle: true,
+        ),
         body: SingleChildScrollView(
           child: Column(
             children: [
-              const Text(
-                "Hello World",
-                style: TextStyle(fontSize: 30, color: Colors.black26),
+              Padding(
+                padding: const EdgeInsets.only(top: 18, left: 18, right: 18),
+                child: Container(
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: TextFormField(
+                      style: const TextStyle(
+                        color: Colors.white,
+                        letterSpacing: 0.8,
+                        fontSize: 16,
+                      ),
+                      decoration: InputDecoration(
+                        label: const Text(
+                          "Shop Owner name",
+                          style: TextStyle(color: Colors.blue, fontSize: 20),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
               ),
-              ElevatedButton(
-                onPressed: () async {
-                  await _auth.signOut();
-                  // ignore: use_build_context_synchronously
-                  Navigator.pop(context);
-                },
-                child: const Text("Logout"),
+              Padding(
+                padding: const EdgeInsets.only(top: 18, left: 18, right: 18),
+                child: Container(
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: TextFormField(
+                      style: const TextStyle(
+                        color: Colors.white,
+                        letterSpacing: 0.8,
+                        fontSize: 16,
+                      ),
+                      decoration: InputDecoration(
+                        label: const Text(
+                          "Shop name",
+                          style: TextStyle(color: Colors.blue, fontSize: 20),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 18, left: 18, right: 18),
+                child: Container(
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: TextFormField(
+                      style: const TextStyle(
+                        color: Colors.white,
+                        letterSpacing: 0.8,
+                        fontSize: 16,
+                      ),
+                      decoration: InputDecoration(
+                        label: const Text(
+                          "Contact Number",
+                          style: TextStyle(color: Colors.blue, fontSize: 20),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 18, left: 18, right: 18),
+                child: Container(
+                  height: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: TextFormField(
+                      style: const TextStyle(
+                        color: Colors.white,
+                        letterSpacing: 0.8,
+                        fontSize: 16,
+                      ),
+                      decoration: InputDecoration(
+                        label: const Text(
+                          "Cafe/Restaurant Address",
+                          style: TextStyle(color: Colors.blue, fontSize: 20),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(5),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 50,
+              ),
+              SizedBox(
+                width: 250,
+                height: 40,
+                child: ElevatedButton.icon(
+                  onPressed: () {},
+                  icon: const Icon(Icons.location_on),
+                  label: const Text("Get my current location"),
+                ),
               )
             ],
           ),
@@ -38,3 +161,26 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////// logout ////////////////////
+//                  ElevatedButton(
+//                 onPressed: () async {
+//                   await _auth.signOut();
+//                   Navigator.pop(context);
+//                 },
+//                 child: const Text("Logout"),
+//               )
