dcad665716679ca4698810a199026af099e8ca53
unknown <shebinpr126@gmail.com>
2022-02-16 11:46:05 +0530

    item_screen

--- lib/Controller/api_services.dart
+++ lib/Controller/api_services.dart
@@ -1,21 +1,31 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:salt_n_pepper_seller/Model/itemmodel.dart';
 import 'package:salt_n_pepper_seller/Model/menu_model.dart';
 
 class ApiServices {
   FirebaseFirestore firebaseFirestore = FirebaseFirestore.instance;
 
   List<Menus> menuItems(QuerySnapshot snapshot) {
-    return snapshot.docs.map((e) {
-      return Menus(
-        aboutMenu: (e.data() as dynamic)["aboutMenu"].toString(),
-        menuID: (e.data() as dynamic)["menuID"].toString(),
-        publishedDate: (e.data() as dynamic)["publishedDate"].toString(),
-        sellerUID: (e.data() as dynamic)["sellerUID"].toString(),
-        thumbnail: (e.data() as dynamic)["thumbnail"].toString(),
-        status: (e.data() as dynamic)["status"].toString(),
-        menuTitle: (e.data() as dynamic)["title"].toString(),
-      );
-    },).toList();
+    return snapshot.docs.map(
+      (e) {
+        return Menus(
+          // ignore: avoid_dynamic_calls
+          aboutMenu: (e.data() as dynamic)["aboutMenu"].toString(),
+          // ignore: avoid_dynamic_calls
+          menuID: (e.data() as dynamic)["menuID"].toString(),
+          // ignore: avoid_dynamic_calls
+          publishedDate: (e.data() as dynamic)["publishedDate"].toString(),
+          // ignore: avoid_dynamic_calls
+          sellerUID: (e.data() as dynamic)["sellerUID"].toString(),
+          // ignore: avoid_dynamic_calls
+          thumbnail: (e.data() as dynamic)["thumbnail"].toString(),
+          // ignore: avoid_dynamic_calls
+          status: (e.data() as dynamic)["status"].toString(),
+          // ignore: avoid_dynamic_calls
+          menuTitle: (e.data() as dynamic)["title"].toString(),
+        );
+      },
+    ).toList();
   }
 
   Stream getMenus(String uid) {
@@ -25,5 +35,34 @@ class ApiServices {
         .collection("menus")
         .snapshots()
         .map(menuItems);
+  }
+
+  List<ItemModel> items(QuerySnapshot snapshot) {
+    return snapshot.docs.map(
+      (e) {
+        return ItemModel(
+          discription: (e.data() as dynamic)["discription"].toString(),
+          aboutItem: (e.data() as dynamic)["aboutItem"].toString(),
+          itemId: (e.data() as dynamic)["itemID"].toString(),
+          publishedDate: (e.data() as dynamic)["publishedDate"].toString(),
+          sellerUID: (e.data() as dynamic)["sellerUID"].toString(),
+          thumbnail: (e.data() as dynamic)["thumbnail"].toString(),
+          status: (e.data() as dynamic)["status"].toString(),
+          title: (e.data() as dynamic)["title"].toString(),
+          size: (e.data() as dynamic)["size"] as Map,
+        );
+      },
+    ).toList();
+  }
+
+  Stream getItems(String uid, String MenuId) {
+    return firebaseFirestore
+        .collection("sellers")
+        .doc(uid)
+        .collection("menus")
+        .doc(MenuId)
+        .collection("items")
+        .snapshots()
+        .map(items);
   }
 }
--- lib/View/itemscreen.dart
+++ lib/View/itemscreen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:salt_n_pepper_seller/Controller/api_services.dart';
+import 'package:salt_n_pepper_seller/Model/itemmodel.dart';
 import 'package:salt_n_pepper_seller/Model/menu_model.dart';
 import 'package:salt_n_pepper_seller/View/Widgets/addnewitem.dart';
 
@@ -11,8 +14,11 @@ class ItemScreen extends StatefulWidget {
 }
 
 class _ItemScreenState extends State<ItemScreen> {
+    ApiServices apiServices = ApiServices();
   @override
   Widget build(BuildContext context) {
+        final maxWidth = MediaQuery.of(context).size.width;
+    final maxHeight = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
@@ -24,7 +30,7 @@ class _ItemScreenState extends State<ItemScreen> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (ctx) =>  AddNewItem(widget.data),
+                    builder: (ctx) => AddNewItem(widget.data),
                   ),
                 );
               },
@@ -35,7 +41,128 @@ class _ItemScreenState extends State<ItemScreen> {
             )
           ],
         ),
-        body: const Center(),
+        body: StreamBuilder(
+          stream: apiServices.getItems(widget.data.sellerUID!, widget.data.menuID!),
+          builder: (context, snapshot) {
+            List<ItemModel> data = [];
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.hasData) {
+              data = snapshot.data as List<ItemModel>;
+
+              return ListView.builder(
+                // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                //     crossAxisCount: 2),
+                physics: const ScrollPhysics(),
+                shrinkWrap: true,
+                itemCount: data.length,
+                itemBuilder: (context, index) {
+                  // controller.popularImages.add(data[index].thumbnail);
+                  return InkWell(
+                    onTap: () {
+                      
+                    },
+                    child: Container(
+                      margin:
+                          const EdgeInsets.only(left: 40, right: 40, top: 20),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                          10,
+                        ),
+                        color: Colors.blue[50],
+                      ),
+                      width: maxWidth,
+                      height: maxHeight * .8,
+                      child: Column(
+                        //  alignment: AlignmentDirectional.bottomCenter,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20),
+                            child: Container(
+                              alignment: Alignment.center,
+                              height: maxHeight * .45,
+                              width: maxWidth * .45,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                boxShadow: [
+                                  BoxShadow(
+                                    offset: const Offset(10.0, 10.0),
+                                    blurRadius: 10,
+                                    spreadRadius: 1,
+                                    color: Colors.blue.shade100,
+                                  ),
+                                  BoxShadow(
+                                    offset: const Offset(-10.0, -10.0),
+                                    blurRadius: 10,
+                                    spreadRadius: 1,
+                                    color: Colors.blue.shade100,
+                                  ),
+                                ],
+                                borderRadius: BorderRadius.circular(100),
+                              ),
+                              child: SizedBox(
+                                width: maxWidth * .4,
+                                height: maxHeight * 0.4,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(100),
+                                  child: Image.network(
+                                    data[index].thumbnail!,
+                                    fit: BoxFit.cover,
+                                    frameBuilder: (
+                                      context,
+                                      child,
+                                      frame,
+                                      wasSynchronouslyLoaded,
+                                    ) =>
+                                        child,
+                                    loadingBuilder:
+                                        (context, child, loadingProgress) {
+                                      if (loadingProgress == null) {
+                                        return child;
+                                      }
+
+                                      return const Center(
+                                        child: CircularProgressIndicator(),
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Text(
+                            data[index].title!,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 0.5,
+                            ),
+                          ),
+                          Text(
+                            data[index].aboutItem!,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                            style: const TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 0.5,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+            return const CircularProgressIndicator();
+          },
+        ),
       ),
     );
   }
