9d236c7ed09c07e68433675e0e1fe67d1e88ebbd
unknown <shebinpr126@gmail.com>
2022-02-16 16:08:03 +0530

    item

--- lib/View/itemscreen.dart
+++ lib/View/itemscreen.dart
@@ -14,10 +14,10 @@ class ItemScreen extends StatefulWidget {
 }
 
 class _ItemScreenState extends State<ItemScreen> {
-    ApiServices apiServices = ApiServices();
+  ApiServices apiServices = ApiServices();
   @override
   Widget build(BuildContext context) {
-        final maxWidth = MediaQuery.of(context).size.width;
+    final maxWidth = MediaQuery.of(context).size.width;
     final maxHeight = MediaQuery.of(context).size.width;
     return SafeArea(
       child: Scaffold(
@@ -42,7 +42,8 @@ class _ItemScreenState extends State<ItemScreen> {
           ],
         ),
         body: StreamBuilder(
-          stream: apiServices.getItems(widget.data.sellerUID!, widget.data.menuID!),
+          stream:
+              apiServices.getItems(widget.data.sellerUID!, widget.data.menuID!),
           builder: (context, snapshot) {
             List<ItemModel> data = [];
             if (snapshot.connectionState == ConnectionState.waiting) {
@@ -51,6 +52,7 @@ class _ItemScreenState extends State<ItemScreen> {
               );
             }
             if (snapshot.hasData) {
+              // ignore: cast_nullable_to_non_nullable
               data = snapshot.data as List<ItemModel>;
 
               return ListView.builder(
@@ -62,99 +64,118 @@ class _ItemScreenState extends State<ItemScreen> {
                 itemBuilder: (context, index) {
                   // controller.popularImages.add(data[index].thumbnail);
                   return InkWell(
-                    onTap: () {
-                      
-                    },
-                    child: Container(
-                      margin:
-                          const EdgeInsets.only(left: 40, right: 40, top: 20),
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(
-                          10,
-                        ),
-                        color: Colors.blue[50],
-                      ),
-                      width: maxWidth,
-                      height: maxHeight * .8,
-                      child: Column(
-                        //  alignment: AlignmentDirectional.bottomCenter,
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.only(top: 20),
-                            child: Container(
-                              alignment: Alignment.center,
-                              height: maxHeight * .45,
-                              width: maxWidth * .45,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
-                                boxShadow: [
-                                  BoxShadow(
-                                    offset: const Offset(10.0, 10.0),
-                                    blurRadius: 10,
-                                    spreadRadius: 1,
-                                    color: Colors.blue.shade100,
+                    onTap: () {},
+                    child: Stack(
+                      children: [
+                        Container(
+                          margin: const EdgeInsets.only(
+                              left: 40, right: 40, top: 20),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(
+                              10,
+                            ),
+                            color: Colors.blue[50],
+                          ),
+                          width: maxWidth,
+                          height: maxHeight * .8,
+                          child: Column(
+                            //  alignment: AlignmentDirectional.bottomCenter,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 20),
+                                child: Container(
+                                  alignment: Alignment.center,
+                                  height: maxHeight * .45,
+                                  width: maxWidth * .45,
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    boxShadow: [
+                                      BoxShadow(
+                                        offset: const Offset(10.0, 10.0),
+                                        blurRadius: 10,
+                                        spreadRadius: 1,
+                                        color: Colors.blue.shade100,
+                                      ),
+                                      BoxShadow(
+                                        offset: const Offset(-10.0, -10.0),
+                                        blurRadius: 10,
+                                        spreadRadius: 1,
+                                        color: Colors.blue.shade100,
+                                      ),
+                                    ],
+                                    borderRadius: BorderRadius.circular(100),
                                   ),
-                                  BoxShadow(
-                                    offset: const Offset(-10.0, -10.0),
-                                    blurRadius: 10,
-                                    spreadRadius: 1,
-                                    color: Colors.blue.shade100,
-                                  ),
-                                ],
-                                borderRadius: BorderRadius.circular(100),
-                              ),
-                              child: SizedBox(
-                                width: maxWidth * .4,
-                                height: maxHeight * 0.4,
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.circular(100),
-                                  child: Image.network(
-                                    data[index].thumbnail!,
-                                    fit: BoxFit.cover,
-                                    frameBuilder: (
-                                      context,
-                                      child,
-                                      frame,
-                                      wasSynchronouslyLoaded,
-                                    ) =>
-                                        child,
-                                    loadingBuilder:
-                                        (context, child, loadingProgress) {
-                                      if (loadingProgress == null) {
-                                        return child;
-                                      }
-
-                                      return const Center(
-                                        child: CircularProgressIndicator(),
-                                      );
-                                    },
+                                  child: SizedBox(
+                                    width: maxWidth * .4,
+                                    height: maxHeight * 0.4,
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.circular(100),
+                                      child: Image.network(
+                                        data[index].thumbnail!,
+                                        fit: BoxFit.cover,
+                                        frameBuilder: (
+                                          context,
+                                          child,
+                                          frame,
+                                          wasSynchronouslyLoaded,
+                                        ) =>
+                                            child,
+                                        loadingBuilder:
+                                            (context, child, loadingProgress) {
+                                          if (loadingProgress == null) {
+                                            return child;
+                                          }
+                                          return const Center(
+                                            child: CircularProgressIndicator(),
+                                          );
+                                        },
+                                      ),
+                                    ),
                                   ),
                                 ),
                               ),
+                              const SizedBox(
+                                height: 15,
+                              ),
+                              Text(
+                                data[index].title!,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 0.5,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                data[index].aboutItem!,
+                                maxLines: 1,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                  letterSpacing: 0.5,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Positioned(
+                          left: 300,
+                          top: 30,
+                          child: IconButton(
+                            onPressed: () {},
+                            icon: Icon(
+                              Icons.delete,
+                              size: 35,
+                              color: Colors.blue[900],
                             ),
                           ),
-                          Text(
-                            data[index].title!,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(
-                              fontSize: 22,
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: 0.5,
-                            ),
-                          ),
-                          Text(
-                            data[index].aboutItem!,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                            style: const TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                              letterSpacing: 0.5,
-                            ),
-                          ),
-                        ],
-                      ),
+                        )
+                      ],
                     ),
                   );
                 },
